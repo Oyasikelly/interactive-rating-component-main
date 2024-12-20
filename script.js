9038370a93390b81d4c18d9c10203626069c62ac
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2024-12-20 09:37:39 +0100

    new update

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-"use strict";
-
-const tabContainer = document.querySelector(".rating");
-const tabs = document.querySelectorAll(".Num");
+// "use strict";
+const wrapper = document.querySelector(".wrapper");
+const Numbs = document.querySelectorAll(".Num");
+const button = document.querySelector(".button");
+const content_end = document.querySelector(".content-end");
 const contents = document.querySelectorAll(".content");
+const select_tab = document.querySelector(".select-tab");
+const select = [];
+Numbs.forEach((numb) => {
+  numb.addEventListener("click", (e) => {
+    // select rating
+    const clicked = e.target.closest(".Num");
+    if (!clicked) return;
+    select.push(clicked.innerHTML);
+    if (select.length > 5) {
+      select.pop();
+    }
+    // Selecting Number
 
-tabContainer.addEventListener("click", (e) => {
-  const clicked = e.target.closest(".Num");
-  console.log(clicked);
-  if (!clicked) return;
+    const content = document.querySelector(`.content--${clicked.dataset.tab}`);
 
-  tabs.forEach((tabs) => tabs.classList.remove("num--active"));
-  contents.forEach((content) => content.classList.remove("content--active"));
-  clicked.classList.add("num--active");
+    contents.forEach((content) => {
+      content.classList.remove("content--active");
+    });
+    content.classList.add("content--active");
+    // content.classList.add("show");
 
-  // active content
-  const activeContent = document.querySelector(
-    `.content--${clicked.dataset.tab}`
-  );
+    if (numb.parentNode.classList.contains("active-color")) {
+      numb.classList.add("active-color");
+    } else {
+      Numbs.forEach((e) => e.parentNode.classList.remove("active-color"));
+      numb.classList.add("active-color");
+    }
+  });
+});
 
-  activeContent.classList.add("content--active");
-  // console.log(clicked.dataset.tab);
+// Add Event To botton
 
-  if (clicked.dataset.tab === "5") tabContainer.style.display = "none";
+button.addEventListener("click", () => {
+  setTimeout(() => {
+    wrapper.classList.add("hidden");
+    content_end.classList.add("show");
+    select_tab.innerHTML = `You selected ${select.length} out of 5 `;
+  }, 2000);
 });
